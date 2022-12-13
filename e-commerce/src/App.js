a0e97ed3e9528c77df4f1e1a65caf69ac7656923
Anida-Assane <anidaassaneyacouba18@gmail.com>
2022-12-13 11:49:03 +0100

    Initialisation du projet

---
+++ e-commerce/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">Store</a>
+      </header>
+      <main>List products</main>
+    </div>
+  );
+}
+
+export default App;
